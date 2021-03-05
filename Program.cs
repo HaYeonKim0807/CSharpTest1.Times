f06845b9e7bb1827861705f921bace64152fc7ae
HaYeonKim0807 <kh701201@naver.com>
2021-03-06 04:53:58 +0900

    Hello

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int a = 1; a <= 9; a++)
+            {
+
+                for (int b = 1; b <= 9; b++)
+                {
+                    int result = a * b;
+                    Console.WriteLine("{0}X{1}={2}", a, b, result);
+
+                }
+
+            }
+
+        }
+    }
+}
